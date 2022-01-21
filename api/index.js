8581c20479bb97c90aa07ca1e10ecd92e81dd372
Saelens Aurelien <aureliensaelens0@gmail.com>
2022-01-21 12:07:03 +0100

    final deployment

--- api/index.js
+++ api/index.js
@@ -49,6 +49,18 @@ app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/posts", postRoute);
 
+if(process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, '/client/build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  })
+} else {
+  app.get('/', (req, res) => {
+    res.send("api running");
+  })
+}
+
 app.listen(8800, () => {
   console.log("Backend server is running!");
 });
